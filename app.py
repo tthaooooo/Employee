10512680725d484794133be5cdcbab4d0c6fc79f
tthaooooo <vuthithuthao3102006@gmail.com>
2025-05-29 15:48:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ for level in visible_levels:
     for status in ['No', 'Yes']:
         for _, row in data[data['Entrepreneurship'] == status].iterrows():
             if row['Percentage'] > 0:
-                y_pos = 0.30 if status == 'No' else 1.00
+                y_pos = 0.15 if status == 'No' else 0.90
                 fig_bar.add_annotation(
                     x=row['Age'],
                     y=y_pos,
